1fc12aa5dd5c9c6aecc2c06b513911a3b2b5190e
ShiroKitsuneLP <shirokitsunelp@gmail.com>
2025-11-17 19:33:30 +0100

    Added a Avatar Command

---
+++ src/commands/Utility/avatar.js
@@ -0,0 +1,31 @@
+// Import necessary discord.js modules
+const { SlashCommandBuilder } = require('discord.js');
+
+// Import necessary modules
+const path = require('path');
+
+// Import embedBuilder
+const { createMiyukiEmbed } = require(path.join(__dirname, './../../utils/embedBuilder'));
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('avatar')
+    .setDescription('Get the avatar of a user')
+    .addUserOption(option => 
+        option.setName('user')
+            .setDescription('The user whose avatar you want to get')
+            .setRequired(false)
+    ),
+    category: 'Utility',
+    usage: '/avatar [user]',
+    async execute(interaction, miyuki) {
+
+        // Get the user option if provided, else use the interaction user
+        const user = interaction.options.getUser('user') || interaction.user;
+
+        return interaction.reply({ embeds: [createMiyukiEmbed(miyuki, {
+            title: `${user.username}'s Avatar`,
+            image: user.displayAvatarURL({ dynamic: true, size: 2048 })
+        })] });
+    }
+}
